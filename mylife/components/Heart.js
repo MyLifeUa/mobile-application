156cbd0abb29411b51af3638b0184d623643772b
TomasCostaK <tomascosta@ua.pt>
2020-03-14 17:38:53 +0000

    baseline structure redefined

---
+++ mylife/components/Heart.js
@@ -0,0 +1,20 @@
+//This is an example code for Bottom Navigation//
+import React from 'react';
+//import react in our code.
+import { 
+    View, 
+    StyleSheet,
+} from 'react-native';
+//import all the basic component we have used
+import styles from '../constants/styles.js';
+
+export const Heart = ({filled,style, ...props}) => {
+    const centerNonFilled = (
+        <View style={}>
+        </View>
+    )
+    return(
+        <View>
+        </View>
+    )
+}
---
+++ mylife/data/users.js
@@ -0,0 +1,26 @@
+var usersList = [
+    {
+        "id": "1",
+        "name": "Joaquim Carlos",
+        "weight": "62.0",
+        "photo": "../assets/icon.png"
+
+    },
+
+    {
+        "id": "2", //user ID
+        "name": "Joao Carlos",
+        "weight": "101.2",
+        "photo": "../assets/icon.png"
+
+    },
+    {
+        "id": "3", //user ID
+        "name": "Carlos Carlos",
+        "weight": "66.3",
+        "photo": "../assets/icon.png"
+
+    },
+
+]
+module.exports = usersList;
