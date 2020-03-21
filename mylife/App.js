78baf58f645e9d33f40f6c44e0271e745ca136c4
TomasCostaK <tomascosta@ua.pt>
2020-03-21 17:03:03 +0000

    Interface done for Login

--- mylife/App.js
+++ mylife/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Profile from './screens/Profile'
+import Login from './screens/Login'
+
 
 export default function App() {
     return (
-        <Profile></Profile>
+        <Login></Login>
     );
 }
 
--- mylife/screens/Login.js
+++ mylife/screens/Login.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { LinearGradient } from 'expo-linear-gradient';
 import theme from '../constants/theme.style.js';
+import { scale,verticalScale, moderateScale } from 'react-native-size-matters';
 
 //import react in our code.
 import {
@@ -9,6 +10,9 @@ import {
     Image,
     StyleSheet,
     ScrollView,
+    TouchableOpacity,
+    TextInput,
+    Text,
     Dimensions
 } from 'react-native';
 const { width, height } = Dimensions.get('screen');
@@ -18,39 +22,112 @@ export default class Login extends React.Component {
   //Detail Screen to show from any Open detail button
   constructor(props) {
     super(props);
+    state = {
+        email:'',
+        password:'',
+    }
+  }
+
+  makeRemoteRequest(){
+      
   }
 
   render() {
     return (
-      <LinearGradient colors={['#C73737', '#EE7777']} start={{ x: 0, y: 0.5 }}
-        end={{ x: 1, y: 0.5 }} style={{flex:1}}>
-        <ScrollView style={{}} contentContainerStyle={{ flexGrow: 1 }}>
-          <View style={{ flex:1}}>
-            <View style={{
-              flex: 2,
-              marginTop: 0.036 * height,
-              alignItems: 'center',
-              justifyContent: 'center',
-           
-            }}>
-
-              < Image style={{
-                width: 250,
-                height: 150,
-                border:1,
-                borderColor:'white',
+        <View style={styles.container}>
+            
+            {/* Imagem e logo */}
+            <Image style={{
+                width: moderateScale(100),
+                height: moderateScale(100),
                 resizeMode: 'contain',
-              }} source={require('../assets/icon.png')} />
+                }} source={require('../assets/icon.png')} />
 
+
+            <View style={styles.inputView} >
+                <TextInput  
+                    style={styles.inputText}
+                    placeholder="Email..." 
+                    placeholderTextColor="#003f5c"
+                    onChangeText={text => this.setState({email:text})}/>
             </View>
-            <View style={{ flex: 1, }}>
+
+            <View style={styles.inputView} >
+                <TextInput  
+                    secureTextEntry
+                    style={styles.inputText}
+                    placeholder="Password..." 
+                    placeholderTextColor="#003f5c"
+                    onChangeText={text => this.setState({password:text})}/>
             </View>
-          </View>
-        </ScrollView>
-      </LinearGradient>
+
+            <TouchableOpacity>
+                <Text style={styles.forgot}>Forgot Password?</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.loginBtn}>
+                <Text style={styles.loginText}>LOGIN</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity>
+                <Text style={styles.loginText}>Signup</Text>
+            </TouchableOpacity>
+
+  
+        </View>
+
     );
   }
 }
 
 const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: theme.primary_color,
+        alignItems: 'center',
+        paddingVertical:20,
+        justifyContent: 'center',
+    },
+
+    inputView:{
+        width:"80%",
+        backgroundColor:theme.white,
+        color:theme.primary_color,
+        borderRadius:20,
+        height:45,
+        marginBottom:20,
+        justifyContent:"center",
+        padding:20
+    },
+    
+    inputText:{
+        height:50,
+        color:theme.primary_color,
+    },
+
+    forgot:{
+        color:theme.white,
+        fontSize:14
+    },
+    
+    loginBtn:{
+        width:"80%",
+        backgroundColor:"#fb5b5a",
+        borderRadius:25,
+        height:50,
+        alignItems:"center",
+        justifyContent:"center",
+        marginTop:40,
+        marginBottom:10
+    },
+
+    loginText:{
+        color:"white"
+    },
+
+    logo_text:{
+        fontSize:theme.h1,
+        color:theme.white,
+        fontWeight:'bold'
+    }
 });
