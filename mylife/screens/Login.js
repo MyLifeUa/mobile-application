ec92643c6063402acbb90835dc81a4f7d81dd71f
TomasCostaK <tomascosta@ua.pt>
2020-04-01 19:16:40 +0100

    comment for later change

--- mylife/screens/Login.js
+++ mylife/screens/Login.js
@@ -60,6 +60,7 @@ export default class Login extends React.Component {
                 }
                 else { 
                     //Navigate to home screen,  with fields: role, Token, data
+                    // So fazer o navigate if json.role == client
                     alert(`Welcome to MyLife, ${json.data.first_name} ${json.data.last_name}!`)
                     console.log(`Role: ${json.role}, with Token: ${json.token}`)
                 }
