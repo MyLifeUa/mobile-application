777cff19494126df22d63ffe63f4866b004e01f7
Tom1k <tomascosta@ua.pt>
2020-04-06 13:09:29 +0100

    baseline Navigation

--- mylife/App.js
+++ mylife/App.js
@@ -3,13 +3,29 @@ import { StyleSheet, Text, View } from 'react-native';
 import Profile from './screens/Profile'
 import Login from './screens/Login'
 import Register from './screens/Register'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 
-export default function App() {
-    return (
-        <Register></Register>
-    );
+function HomeScreen() {
+  return (
+    <Login></Login>
+  );
 }
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
