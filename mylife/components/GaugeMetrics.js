e6e5fe20cd01902565c44220f270d6e0c733d841
Tom1k <tomascosta@ua.pt>
2020-05-02 23:06:08 +0100

    Baseline custom gauge for metrics

--- mylife/components/GaugeMetrics.js
+++ mylife/components/GaugeMetrics.js
@@ -44,17 +44,26 @@ export default class GaugeMetrics extends React.Component {
   constructor(props) {
     super(props);
   }
-
+  n_labels = 5
   state = {
     index: 0,
     routes: [
       { key: "first", title: "Body" },
       { key: "second", title: "Nutrients" }
     ],
+    labels_sizes : [1/this.n_labels, 1/this.n_labels, 1/this.n_labels, 1/this.n_labels, 1/this.n_labels]
   };
 
   componentDidMount(){
+    //receives 
+    //array with values
+    //value of metric
+    //label done
     console.log(this.props.labels)
+  }
+
+  calculateDiffRanges(){
+    //returns array from state labels, with 5 values ordered and their flex percentage
   }
 
   render() {
@@ -62,14 +71,16 @@ export default class GaugeMetrics extends React.Component {
       <View style={{
         backgroundColor: theme.white,
         flex: 1,
-        alignContent: "space-between"
+        alignContent: "space-between",
+        paddingHorizontal:moderateScale(15),
+        overflow: "hidden"
       }}>
           <View
             style={{
               flexDirection: "row",
               justifyContent: "center",
               alignContent: "center",
-              marginTop: moderateScale(20)
+              marginTop: moderateScale(20),
             }}
           >
             <Text
@@ -79,21 +90,65 @@ export default class GaugeMetrics extends React.Component {
                 fontWeight: "bold"
               }}
             >
-              Clients Metrics
+              Heart Rate
             </Text>
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "center",
+              alignContent: "center",
               
-            
+            }}
+          >
+            <Text
+              style={{
+                fontSize: theme.body,
+                color: 'gray',
+                fontWeight: "bold"
+              }}
+            >
+              Your estimate of 52 is 
+            </Text>
+            <Text
+              style={{
+                fontSize: theme.body,
+                color: "#80ccff",
+                fontWeight: "bold",
+                paddingHorizontal: moderateScale(5)
+              }}
+            >
+               Good
+            </Text>
+            <Text
+              style={{
+                fontSize: theme.body,
+                color: 'gray',
+                fontWeight: "bold",
+              }}
+            >
+               for men your age
+            </Text>
           </View>
           <View 
                 style={{    
                     flexDirection: "row",
                     justifyContent: "center",
                     alignContent: "center",
+                    backgroundColor:'white',
+                    flex:1,
+                    marginTop:verticalScale(15),
+                    maxHeight:verticalScale(15),
+                    borderRadius: 10,
+                    overflow: "hidden"
                 }}
             >
-                <RNSpeedometer labels={this.props.labels} minValue={50} maxValue={130} value={this.props.value} size={200}/>
-            </View>
-          
+              <View style={{flex:1,backgroundColor:'#bb99ff'}}></View>
+              <View style={{flex:1.5,backgroundColor:'#80ccff'}}></View>
+              <View style={{flex:2,backgroundColor:'#99ffff'}}></View>
+              <View style={{flex:2,backgroundColor:'#d9ffb3'}}></View>
+              <View style={{flex:0.8,backgroundColor:'#99ff33'}}></View>
+          </View>
       </View>
     );
   }
--- mylife/screens/Profile.js
+++ mylife/screens/Profile.js
@@ -61,33 +61,7 @@ export default class Login extends React.Component {
         sedentaryMinutes: null,
         steps: null
       },
-      labels: [
-        {
-          name: 'Excellent',
-          labelColor: '#ff2900',
-          activeBarColor: '#ff2900',
-        },
-        {
-          name: 'Good',
-          labelColor: '#ff5400',
-          activeBarColor: '#ff5400',
-        },
-        {
-          name: 'Average',
-          labelColor: '#f4ab44',
-          activeBarColor: '#f4ab44',
-        },
-        {
-          name: 'Fair',
-          labelColor: '#f2cf1f',
-          activeBarColor: '#f2cf1f',
-        },
-        {
-          name: 'Poor',
-          labelColor: '#14eb6e',
-          activeBarColor: '#14eb6e',
-        },
-      ],
+      
     };
   }
   _storeData = async token => {
@@ -420,7 +394,7 @@ export default class Login extends React.Component {
           showsPagination={true}
           
           loop={false}>
-          <GaugeMetrics navigation={this.props.navigation} value={54} labels={this.state.labels}/>
+          <GaugeMetrics navigation={this.props.navigation} value={54}/>
           <View style={{flex:1}}>
             <ScrollView
             style={{
