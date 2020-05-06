74a7cd4ab25159f67fe435900db244608787c7d7
jmnmv12 <j.vasconcelos99@gmail.com>
2020-05-06 17:49:06 +0100

    Notifications finished

--- mylife/components/HeaderRightNavBar.js
+++ mylife/components/HeaderRightNavBar.js
@@ -1,197 +1,196 @@
 //This is an example code for Bottom Navigation//
-import React from 'react';
+import React from "react";
 //import react in our code.
-import { Text, View, TouchableOpacity, StyleSheet,Image,AsyncStorage,ActivityIndicator } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-import NavigationService from '../components/NavigationService'
-
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+  AsyncStorage,
+  ActivityIndicator
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import NavigationService from "../components/NavigationService";
+const API_URL = "http://mednat.ieeta.pt:8442";
 
 //import all the basic component we have used
- 
+
 export default class HeaderRightNavBar extends React.Component {
   //Detail Screen to show from any Open detail button
 
   constructor(props) {
     super(props);
     this.state = {
-      SharedLoading:true,
-      userName:null,
-      userPassword:null,
-      userToken:null,
-      userCode:null,
-      baseURL:"http://192.168.160.220:8080/"
+      SharedLoading: true,
 
-    
-
-      
+      user_token: null,
+      expo_token: null
     };
   }
 
   _deleteData = async () => {
-    
     try {
-      await AsyncStorage.removeItem('token');
-      await AsyncStorage.removeItem('email');
-      
-
-
-     
-    }
-    catch(exception) {
+      await AsyncStorage.removeItem("token");
+      await AsyncStorage.removeItem("email");
+      await AsyncStorage.removeItem("expo_token");
+    } catch (exception) {
       return false;
     }
-
   };
 
-  async logoutWithoutNotifications() {
+  async logoutWithNotifications() {
+    await this._deleteData();
+    await this.deleteToken();
+    
+  }
 
-    this._deleteData();
-    NavigationService.navigate('Auth');
+  async processInvalidToken() {
+    this._removeData();
+    this.props.navigation.navigate("Auth");
+  }
 
+  _removeData = async () => {
+    // TODO Remove Fitbit flag
+  };
+
+  async processResponse(response) {
+    console.log("Hello");
+    const statusCode = response.status;
+    const data = response.json();
+    const res = await Promise.all([statusCode, data]);
+    return {
+      statusCode: res[0],
+      responseJson: res[1]
+    };
   }
 
   async deleteToken() {
-    console.log(this.state.userToken)
-    const { userCode, userToken,baseURL,userName,userPassword } = this.state;
+    //unsecure way to send a post
+    var login_info = "Token " + this.state.user_token;
 
-    login_info = "Basic " + Base64.btoa(userName + ":" + userPassword);
-
-    var details = {
-      'token': userToken,
-    }
-
-    // fetch data
-    fetch(baseURL + "api/v1/expo_tokens/"+userCode, {
-        method: 'DELETE',
+    if (this.state.expo_token != null) {
+      fetch(`${API_URL}/expo-tokens`, {
+        method: "DELETE",
         headers: {
           "Content-Type": "application/json",
-          "Authorization": login_info
+          Authorization: login_info
         },
-        body: JSON.stringify(details)
-
-    })
-    .then(response => {
-      if (!response.ok) throw new Error(response.status);
-      else return response.json();
-    })
-    .then(data => {
-      console.log(data)
-
-      this._deleteData();
-      NavigationService.navigate('Login');
+        body: JSON.stringify({
+          expo_token: this.state.expo_token
+        })
+      })
+        .then(this.processResponse)
+        .then(res => {
+          const { statusCode, responseJson } = res;
+          console.log(responseJson);
 
 
-
-
-      
-
-      //INSERT LOGOUT LOGIC HERE
-      
-    })
-    .catch(error => {
-      console.log("error: " + error);
-    });
-  
+          if (statusCode == 401) {
+            this.processInvalidToken();
+          } else {
+            NavigationService.navigate("Auth");
+          }
+        })
+        .catch(error => {
+          //this.handleRefresh()
+          alert("Error deleting expo token.");
+          console.error(error);
+        });
+    }
   }
 
   _retrieveData = async () => {
-    
+    console.log("HELLO");
 
     try {
-      const userName= await AsyncStorage.getItem('username', this.state.userName);
-      const pwd= await AsyncStorage.getItem('pwd', this.state.userPassword);
-      const value = await AsyncStorage.getItem('cod');
-      const token = await AsyncStorage.getItem('token');
+      const value = await AsyncStorage.getItem("token");
+      const expo_token = await AsyncStorage.getItem("expo_token", null);
 
-      if (value !== null && pwd !==null && userName !==null && token!==null) {
+      if (value !== null) {
         // We have data!!
         this.setState({
           SharedLoading: false,
-          userCode: value,
-          userToken:token,
-          userName:userName,
-          userPassword:pwd,
-          isLoading:true,
-          
+          user_token: value,
+          expo_token: expo_token
+        });
+      } else {
+        this.setState({
+          SharedLoading: false // TODO ELIMINATE THIS
         });
       }
     } catch (error) {
       console.log(error);
+      this.setState({
+        SharedLoading: false // TODO ELIMINATE THIS
+      });
     }
   };
 
-
   async componentDidMount() {
-
-    //this._retrieveData();
-    
+    this._retrieveData();
   }
 
- 
-
   render() {
-    
     return (
-        <View style={{flex:1,flexDirection:'row'}}>
-
-          <TouchableOpacity
-            onPress={this.logoutWithoutNotifications.bind(this)}
-          >
-            <View style={{ paddingHorizontal: 15 }}>
-             
-              
-                <Ionicons name="md-exit" size={32} color="white"/>
-              
-            </View>
-          </TouchableOpacity>
-          
-
+      <View style={{ flex: 1, flexDirection: "row" }}>
+        <TouchableOpacity onPress={this.logoutWithNotifications.bind(this)}>
+          <View style={{ paddingHorizontal: 15 }}>
+            <Ionicons name="md-exit" size={32} color="white" />
+          </View>
+        </TouchableOpacity>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-    icons: {
-      paddingHorizontal:15
-    }
-  });
+  icons: {
+    paddingHorizontal: 15
+  }
+});
 
-
-const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+const chars =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
 const Base64 = {
-  btoa: (input:string = '')  => {
+  btoa: (input: string = "") => {
     let str = input;
-    let output = '';
+    let output = "";
 
-    for (let block = 0, charCode, i = 0, map = chars;
-    str.charAt(i | 0) || (map = '=', i % 1);
-    output += map.charAt(63 & block >> 8 - i % 1 * 8)) {
+    for (
+      let block = 0, charCode, i = 0, map = chars;
+      str.charAt(i | 0) || ((map = "="), i % 1);
+      output += map.charAt(63 & (block >> (8 - (i % 1) * 8)))
+    ) {
+      charCode = str.charCodeAt((i += 3 / 4));
 
-      charCode = str.charCodeAt(i += 3/4);
-
-      if (charCode > 0xFF) {
-        throw new Error("'btoa' failed: The string to be encoded contains characters outside of the Latin1 range.");
+      if (charCode > 0xff) {
+        throw new Error(
+          "'btoa' failed: The string to be encoded contains characters outside of the Latin1 range."
+        );
       }
-      
-      block = block << 8 | charCode;
+
+      block = (block << 8) | charCode;
     }
-    
+
     return output;
   },
 
-  atob: (input:string = '') => {
-    let str = input.replace(/=+$/, '');
-    let output = '';
+  atob: (input: string = "") => {
+    let str = input.replace(/=+$/, "");
+    let output = "";
 
     if (str.length % 4 == 1) {
-      throw new Error("'atob' failed: The string to be decoded is not correctly encoded.");
+      throw new Error(
+        "'atob' failed: The string to be decoded is not correctly encoded."
+      );
     }
-    for (let bc = 0, bs = 0, buffer, i = 0;
-      buffer = str.charAt(i++);
-
-      ~buffer && (bs = bc % 4 ? bs * 64 + buffer : buffer,
-        bc++ % 4) ? output += String.fromCharCode(255 & bs >> (-2 * bc & 6)) : 0
+    for (
+      let bc = 0, bs = 0, buffer, i = 0;
+      (buffer = str.charAt(i++));
+      ~buffer && ((bs = bc % 4 ? bs * 64 + buffer : buffer), bc++ % 4)
+        ? (output += String.fromCharCode(255 & (bs >> ((-2 * bc) & 6))))
+        : 0
     ) {
       buffer = chars.indexOf(buffer);
     }
--- mylife/screens/Login.js
+++ mylife/screens/Login.js
@@ -1,26 +1,27 @@
 //This is an example code for Bottom Navigation//
-import React from 'react';
-import { LinearGradient } from 'expo-linear-gradient';
-import theme from '../constants/theme.style.js';
-import { scale,verticalScale, moderateScale } from 'react-native-size-matters';
+import React from "react";
+import { LinearGradient } from "expo-linear-gradient";
+import theme from "../constants/theme.style.js";
+import { scale, verticalScale, moderateScale } from "react-native-size-matters";
+import * as Permissions from 'expo-permissions'
+import {Notifications } from 'expo'
+
 
 //import react in our code.
 import {
-    View,
-    Image,
-    StyleSheet,
-    ScrollView,
-    TouchableOpacity,
-    TextInput,
-    Text,
-    Dimensions,
-    AsyncStorage
-} from 'react-native';
-const { width, height } = Dimensions.get('screen');
+  View,
+  Image,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+  TextInput,
+  Text,
+  Dimensions,
+  AsyncStorage
+} from "react-native";
+const { width, height } = Dimensions.get("screen");
 //import all the basic component we have used
-const API_URL = 'http://mednat.ieeta.pt:8442';
-
-
+const API_URL = "http://mednat.ieeta.pt:8442";
 
 export default class Login extends React.Component {
   //Detail Screen to show from any Open detail button
@@ -29,161 +30,212 @@ export default class Login extends React.Component {
   }
 
   state = {
-    email:'',
-    password:'',
+    email: "",
+    password: "",
+    expo_token:""
+  };
+
+  componentDidMount() {
+    this.register();
   }
 
-  componentDidMount(){
-    
-  }
+  register = async () => {
+    const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
 
-  _storeData = async (token) => {
-    console.log("Storing Token: "+token)
-    try {
-        await AsyncStorage.setItem('token', token);
-        await AsyncStorage.setItem('email', this.state.email);
-        this.setState({user_token:token})
-  
-    } catch (error) {
-        console.log(error)
-    }
-};
+    if (status !== "granted") {
+      console.log("Permission not granted!");
 
-
-  makeLoginRequest(){
-      //unsecure way to send a post
-    if (this.state.email=='') {
-        alert("Fill in the login information")
+      //this.removeNotificacao(token)
     } else {
-        console.log("Fetching:" + `${API_URL}/login`)
-        fetch(`${API_URL}/login`, {
-            method: 'POST',
-            headers: {
-              Accept: 'application/json',
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-              username: this.state.email,
-              password: this.state.password,
-            }),
-          })
-            .then((response) => response.json())
-            .then((json) => {
-                console.log(json);
-                if (json.detail != undefined){
-                    //Credentials incorrect
-                    alert("Login Credentials are invalid.")
-                }
-                else { 
-                    this._storeData(json.token)
-                    
-                    this.props.navigation.navigate('Profile')
-                }
-            })
-            .catch((error) => {
-                alert("Error fetching login")
-                console.error(error);
-            });
+      try {
+        const token = await Notifications.getExpoPushTokenAsync();
+        console.log("granted", token);
+        this.setState({
+          expo_token: token
+        });
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  };
+
+  _storeData = async token => {
+    console.log("Storing Token: " + token);
+    try {
+      await AsyncStorage.setItem("token", token);
+      await AsyncStorage.setItem("email", this.state.email);
+      await AsyncStorage.setItem('expo_token', this.state.expo_token);
+      this.setState({ user_token: token });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  async insertToken(token) {
+    var login_info = "Token " + token;
+    fetch(`${API_URL}/expo-tokens`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: login_info
+
+      },
+      body: JSON.stringify({
+        expo_token: this.state.expo_token
+      })
+    })
+      .then(response => response.json())
+      .then(json => {
+        console.log(json);
+        this.props.navigation.navigate("Profile");
+      })
+      .catch(error => {
+        alert("Error fetching login");
+        console.error(error);
+      });
+  }
+
+  async makeLoginRequest() {
+    //unsecure way to send a post
+    if (this.state.email == "") {
+      alert("Fill in the login information");
+    } else {
+      console.log("Fetching:" + `${API_URL}/login`);
+      fetch(`${API_URL}/login`, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          username: this.state.email,
+          password: this.state.password
+        })
+      })
+        .then(response => response.json())
+        .then(json => {
+          console.log(json);
+          if (json.detail != undefined) {
+            //Credentials incorrect
+            alert("Login Credentials are invalid.");
+          } else {
+            
+            this._storeData(json.token);
+            this.insertToken(json.token);
+
+            
+          }
+        })
+        .catch(error => {
+          alert("Error fetching login");
+          console.error(error);
+        });
     }
   }
 
   render() {
     return (
-        <View style={styles.container}>
-            
-            {/* Imagem e logo */}
-            <Image style={{
-                width: moderateScale(100),
-                height: moderateScale(100),
-                resizeMode: 'contain',
-                }} source={require('../assets/icon.png')} />
+      <View style={styles.container}>
+        {/* Imagem e logo */}
+        <Image
+          style={{
+            width: moderateScale(100),
+            height: moderateScale(100),
+            resizeMode: "contain"
+          }}
+          source={require("../assets/icon.png")}
+        />
 
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.inputText}
+            placeholder="Email..."
+            placeholderTextColor="#003f5c"
+            onChangeText={text => this.setState({ email: text })}
+          />
+        </View>
 
-            <View style={styles.inputView} >
-                <TextInput  
-                    style={styles.inputText}
-                    placeholder="Email..." 
-                    placeholderTextColor="#003f5c"
-                    onChangeText={text => this.setState({email:text})}/>
-            </View>
+        <View style={styles.inputView}>
+          <TextInput
+            secureTextEntry
+            style={styles.inputText}
+            placeholder="Password..."
+            placeholderTextColor="#003f5c"
+            onChangeText={text => this.setState({ password: text })}
+          />
+        </View>
 
-            <View style={styles.inputView} >
-                <TextInput  
-                    secureTextEntry
-                    style={styles.inputText}
-                    placeholder="Password..." 
-                    placeholderTextColor="#003f5c"
-                    onChangeText={text => this.setState({password:text})}/>
-            </View>
-
-            {/* Forgot password has no use for now
+        {/* Forgot password has no use for now
             <TouchableOpacity>
                 <Text style={styles.forgot}>Forgot Password?</Text>
             </TouchableOpacity>
             */}
-            <TouchableOpacity style={styles.loginBtn} onPress={() => this.makeLoginRequest()}>
-                <Text style={styles.loginText}>LOGIN</Text>
-            </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.loginBtn}
+          onPress={() => this.makeLoginRequest()}
+        >
+          <Text style={styles.loginText}>LOGIN</Text>
+        </TouchableOpacity>
 
-            <TouchableOpacity onPress={() => this.props.navigation.navigate('Register')}>
-                <Text style={styles.loginText}>Signup</Text>
-            </TouchableOpacity>
-
-  
-        </View>
-
+        <TouchableOpacity
+          onPress={() => this.props.navigation.navigate("Register")}
+        >
+          <Text style={styles.loginText}>Signup</Text>
+        </TouchableOpacity>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        backgroundColor: theme.primary_color,
-        alignItems: 'center',
-        paddingVertical:20,
-        justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: theme.primary_color,
+    alignItems: "center",
+    paddingVertical: 20,
+    justifyContent: "center"
+  },
 
-    inputView:{
-        width:"80%",
-        backgroundColor:theme.white,
-        color:theme.primary_color,
-        borderRadius:20,
-        height:45,
-        marginBottom:20,
-        justifyContent:"center",
-        padding:20
-    },
-    
-    inputText:{
-        height:50,
-        color:theme.primary_color,
-    },
+  inputView: {
+    width: "80%",
+    backgroundColor: theme.white,
+    color: theme.primary_color,
+    borderRadius: 20,
+    height: 45,
+    marginBottom: 20,
+    justifyContent: "center",
+    padding: 20
+  },
 
-    forgot:{
-        color:theme.white,
-        fontSize:14
-    },
-    
-    loginBtn:{
-        width:"80%",
-        backgroundColor:"#fb5b5a",
-        borderRadius:25,
-        height:50,
-        alignItems:"center",
-        justifyContent:"center",
-        marginTop:40,
-        marginBottom:10
-    },
+  inputText: {
+    height: 50,
+    color: theme.primary_color
+  },
 
-    loginText:{
-        color:"white"
-    },
+  forgot: {
+    color: theme.white,
+    fontSize: 14
+  },
 
-    logo_text:{
-        fontSize:theme.h1,
-        color:theme.white,
-        fontWeight:'bold'
-    }
+  loginBtn: {
+    width: "80%",
+    backgroundColor: "#fb5b5a",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    marginBottom: 10
+  },
+
+  loginText: {
+    color: "white"
+  },
+
+  logo_text: {
+    fontSize: theme.h1,
+    color: theme.white,
+    fontWeight: "bold"
+  }
 });
